bc7db7b54b8ee5cc3a14511634d03e7b6cd4306c
eunjineunjin <45138692+eunjineunjin@users.noreply.github.com>
2020-06-15 19:54:17 +0900

    어플 소스코드

---
+++ UserApp/MainActivity.java
@@ -0,0 +1,83 @@
+﻿package detect.animal.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    List<String> data;
+    ArrayAdapter<String> adapter;
+    public static Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+
+        list = (ListView)findViewById(R.id.list_view1);
+        data = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+    }
+
+    public void btn (View v) {
+        Parser parser = new Parser();
+        parser.execute();
+    }
+
+    protected String getTime() {
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date time = new Date();
+        String now = format1.format(time);
+
+        return now;
+    }
+
+    protected void AddData (String message) {
+        data.add(message);
+        adapter.notifyDataSetChanged();
+    }
+
+    private class Parser extends AsyncTask<Void, Void, Void> {
+        String content;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String URL = "URL 입력";
+                Document doc = Jsoup.connect(URL).get();
+                content = doc.text();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            AddData(content);
+        }
+    }
+}
